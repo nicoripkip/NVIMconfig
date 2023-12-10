2c0bba09f762fa80c0a863594ea356d6ca938282
Niko van Ommen <nicovanommen.nvo@gmail.comO>
2023-12-10 20:02:53 +0100

    Added more remaps

--- lua/random_plugin/remap.lua
+++ lua/random_plugin/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>b", vim.cmd.ls)
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
