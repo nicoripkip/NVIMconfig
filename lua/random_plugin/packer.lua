83aba93712ff0548f6edf2215aeb2b38ec55a0d0
Niko van Ommen <nicovanommen.nvo@gmail.comO>
2023-12-10 16:57:57 +0100

    Added kanagawa colorscheme

--- lua/random_plugin/packer.lua
+++ lua/random_plugin/packer.lua
@@ -4,4 +4,17 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use {
+  	'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	-- 	or                            , branch = '0.1.x',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ({
+	  "rebelot/kanagawa.nvim", 
+	  config = function() 
+		  vim.cmd("colorscheme kanagawa") 
+	  end 
+  })
+
 end)
